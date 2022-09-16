0604e66169b93f272533fbfcb89ca59275ba53b9
Benedik Hidalgo <9007@htl.rennweg.at>
2022-09-16 13:14:07 +0200

    16.09.2022 Linearer Gradient funktioniert endlich

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-const chart = document.getElementById("myChart").getContext("2d");
+let width, height, gradient;
+function getGradient(ctx, chartArea) {
+  const chartWidth = chartArea.right - chartArea.left;
+  const chartHeight = chartArea.bottom - chartArea.top;
+    // Create the gradient because this is either the first render
+    // or the size of the chart has changed
+    width = chartWidth;
+    height = chartHeight;
+    gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+    gradient.addColorStop(0, "blue");
+    gradient.addColorStop(0.7, "orange");
+    gradient.addColorStop(1, "red");
+
+    console.log(gradient)
+
+  return gradient;
+}
 
 const labels = [
     "Montag",
@@ -8,14 +24,27 @@ const labels = [
     "Fretiag",
     "Samstag",
     "Sonntag",
+    "Montag",
+    "Dienstag",
 ];
 
 const data = {
     labels,
     datasets: [{
-        data: [0, 5, 10, 15, 20, 25, 30, 35, 40],
-        label: "Temperature"
-    }]
+        data: [0, 28, 20, 7, 16, 13, 25, 12, 22],
+        label: "Temperature",
+        //borderColor: "#FF0000",
+        borderColor: function(context) {
+            const chart = context.chart;
+            const {ctx, chartArea} = chart;
+            if (!chartArea) {
+                return;
+            }
+            return getGradient(ctx, chartArea);
+        },
+        pointRadius: "6",       //Punktgröße
+        tension: 0,             //Kurve
+    }],
 };
 
 const config = {
@@ -23,7 +52,40 @@ const config = {
     data: data,
     options: {
         responsive: true,
-    },
+        maintainAspectRatio: false,
+        scales: {
+            yAxis: {
+                grid: {
+                    color: "#FFFFFF40",
+                    tickColor: "#FFFFFF",
+                    borderColor: "#FFFFFF"
+                },
+                ticks: {
+                    color:"#FFFFFF"
+                },
+            },
+            xAxis: {
+                grid: {
+                    color: "#FFFFFF40",
+                    tickColor: "#FFFFFF",
+                    borderColor: "#FFFFFF"
+                },
+                ticks: {
+                    color:"#FFFFFF"
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    color: "#FFFFFF"
+                }
+            }
+        },
+        hover: {
+            mode:false,
+        }
+    }
 };
 
 const myChart = new Chart(document.getElementById("myChart"), config);
